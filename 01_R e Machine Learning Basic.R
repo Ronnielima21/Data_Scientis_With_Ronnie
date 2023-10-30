f9fd444b19337e3a6ef27d4a22e7e0173cbbde43
Data_With_Ronnie <86759323+Ronnielima21@users.noreply.github.com>
2023-10-30 00:04:59 +0000

    Versao_01
    Versao_1

---
+++ 01_R e Machine Learning Basic.R
@@ -0,0 +1,64 @@
+#R e Machine Learning Basic - Ronnie
+
+# Tópicos:
+# 01 - Definições Teóricas de Machine Learning
+# 02 - Definições Teóricas do R e RStudio
+# 03 - Estrutura de operação no R
+# 04 - Conceitos Mínimos de manipulação de dados
+# 05 - Interações e Funções
+# 06 - Data Visualization
+
+# A finalidade deste capítulo é apresentar os conceitos básicos de Machine 
+# Learning, assim como as principais operações no RStudio.
+
+# ------------------------------------------------------------------------------
+
+# Definições Teóricas de Machine Learning
+
+# Machine Learning (Aprendizado de Máquina) é um ramo da inteligência artificial
+# que se concentra no desenvolvimento de algoritmos e modelos estatísticos que 
+# permitem a um sistema aprender e melhorar a performance em uma tarefa 
+# específica à medida que são expostos a mais dados. Em vez de serem 
+# explicitamente programados para realizar uma tarefa, os sistemas de Machine 
+# Learning usam padrões nos dados para aprender como executar a tarefa.
+
+# O processo de aprendizado de máquina geralmente envolve os seguintes passos:
+
+# Coleta de Dados: Reúne-se um conjunto de dados que servirá como entrada 
+# para o modelo de aprendizado.
+
+# Pré-processamento de Dados: Os dados podem precisar ser limpos, normaliza-
+# dos ou transformados de alguma forma para melhorar a qualidade e a utilidade 
+#para o modelo.
+
+# Seleção do Modelo: Escolhe-se um algoritmo ou tipo de modelo a ser usado 
+# com base na natureza da tarefa (classificação, regressão, clustering, etc.) e
+#nos dados disponíveis.
+
+# Treinamento do Modelo: O modelo é alimentado com os dados de treinamento,
+# ajustando seus parâmetros internos para minimizar um erro ou função de custo.
+
+# Avaliação do Modelo: O modelo é testado em dados que não foram usados du-
+# rante o treinamento para avaliar sua capacidade de generalização.
+
+# Ajuste (Opcional): Com base nos resultados da avaliação, os hiperparâme-
+# tros do modelo ou o próprio modelo podem ser ajustados para melhorar o desem-
+# penho.
+
+
+# O objetivo final do Machine Learning é criar modelos capazes de fazer previ- 
+# sões ou tomar decisões precisas com base em novos dados não vistos anterior-
+# mente. Isso é especialmente útil em cenários onde a programação tradicional
+# seria impraticável ou extremamente complexa. Por exemplo, em reconhecimento de
+# voz, diagnóstico médico, recomendação de produtos, entre muitos outros.
+
+
+
+
+
+
+
+
+
+
+
